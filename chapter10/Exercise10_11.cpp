5582a6642110570cfee4fcd56b5f1286d3505e2b
Tui Popenoe <tuipopenoe@gmail.com>
2014-05-20 23:59:30 -0500

    Chapter 10 update

--- chapter10/Exercise10_11.cpp
+++ chapter10/Exercise10_11.cpp
@@ -6,11 +6,36 @@
 
 using namespace std;
 
+float convertToFarenheit(float degrees);
+float convertToCelsius(float degrees);
+
 int main(){
 
     float temp;
     float conv;
+    char type;
     string input;
+    string strtemp;
+    cout << "Enter temperature to convert: ";
+    getline(cin, input);
+    strtemp = input.substr(0, 4);
+    temp = stof(strtemp);
+    type = input.at(4);
 
+    cout << "Converted temperature is ";
+    if(type == 'F'){
+        cout << convertToCelsius(temp) << " degrees Celsius" << endl;
+    }
+    else{
+        cout << convertToFarenheit(temp) << " degrees Farenheit" << endl;
+    }
     return 0;
+}
+
+float convertToCelsius(float degrees){
+    return (5.0 / 9.0) * (degrees - 32.0);
+}
+
+float convertToFarenheit(float degrees){
+    return (9.0 / 5.0) * (degrees + 32.0);
 }
